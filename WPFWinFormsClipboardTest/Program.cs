7d530e1938296d0282f918d3905a08684d270688
Miguel Pena Garcia <miguep@microsoft.com>
2019-04-18 16:13:57 -0700

    Sample app for testing WPF and WinForms clipboard interoperability

---
+++ WPFWinFormsClipboardTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using WPF = System.Windows;
+using WinForms = System.Windows.Forms;
+
+namespace WPFWinFormsClipboardTest
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Registers the custom format if it's not already registered.
+            string customFormatName = "CustomFormat";
+            WPF.DataFormat registeredFormat = WPF.DataFormats.GetDataFormat(customFormatName);
+            Console.WriteLine(String.Format("WPF registered format \'{0}\', got id \'{1}\'", customFormatName, registeredFormat.Id));
+
+            // Set data using the custom format.
+            string dataToWrite = "test data";
+            WPF.Clipboard.SetData(registeredFormat.Name, dataToWrite);
+            // Flush data to clipboard.
+            WPF.Clipboard.Flush();
+
+            Console.WriteLine(String.Format("WPF wrote \'{0}\' to clipboard", dataToWrite));
+
+            // Get the custom format using the id from the registered custom format.
+            System.Windows.Forms.DataFormats.Format format = System.Windows.Forms.DataFormats.GetFormat(registeredFormat.Id);
+            Console.WriteLine(String.Format("Winforms got format data using id from WPF registered format, got \'{0}\'", format.Name));
+            // Get the data under that format.
+            string data = System.Windows.Forms.Clipboard.GetData(format.Name) as string;
+            Console.WriteLine(String.Format("Winforms got data from clipboard, got \'{0}\'", data));
+        }
+    }
+}
